fe485e72338fffb742ebb18d7f5db6d795f3949a
PanenEpam <43788313+PanenEpam@users.noreply.github.com>
2020-04-05 14:50:22 +0300

    Fixed task

--- Java/src/main/java/MailTest.java
+++ Java/src/main/java/MailTest.java
@@ -7,68 +7,72 @@ import org.testng.Assert;
 import java.util.concurrent.TimeUnit;
 
 public class MailTest {
+    WebDriver driver;
+    String addressee;
+    String topic;
+    String text;
+
 
     @BeforeSuite
     public void setUp() {
-        System.setProperty("webdriver.chrome.driver", "D:/chromedriver.exe");
-        // WebDriver driver = ConnectionDriver.openConnection(WebBrowsers.CHROME);
+        System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
     }
 
     @BeforeClass(description = "open browser")
     public void openBrowser() {
-        WebDriver driver = ConnectionDriver.openConnection(WebBrowsers.CHROME);
+        driver = ConnectionDriver.openConnection(WebBrowsers.CHROME);
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
         driver.get("http://mail.ru/");
     }
 
-    @Test(description = "login to mail service", dataProvider = "loginData", dataProviderClass = DataProviders.class)
-    public void doLogin(String login, String password) {
-        System.setProperty("webdriver.chrome.driver", "D:/chromedriver.exe");
 
-        WebDriver driver = ConnectionDriver.openConnection(WebBrowsers.CHROME);
-        driver.findElement(By.id("mailbox:login")).sendKeys(login);
+    @Test(description = "login to mail service")
+    public void doLogin() {
+
+        driver.findElement(By.id("mailbox:login")).sendKeys("epam_homework");
         driver.findElement(By.id("mailbox:submit")).click();
         driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
-        driver.findElement(By.id("mailbox:password")).sendKeys(password);
+        driver.findElement(By.id("mailbox:password")).sendKeys("helloepam123");
         driver.findElement(By.id("mailbox:submit")).click();
     }
 
     @Test(description = "letter creation", dataProvider = "createData", dataProviderClass = DataProviders.class,
             dependsOnMethods = {"doLogin"})
     public void createLetter(String addressee, String topic, String text) {
-        WebDriver driver = ConnectionDriver.openConnection(WebBrowsers.CHROME);
+
+        this.addressee = addressee;
+        this.topic = topic;
+        this.text = text;
 
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-        driver.findElement(By.xpath("//span[@title='Написать письмо']/span")).click();
+        driver.findElement(By.xpath("//a[@title='Написать письмо']")).click();
         driver.findElement(By.xpath("//input[@class='container--H9L5q size_s--3_M-_']")).sendKeys(addressee);
         driver.findElement(By.xpath("//input[@name='Subject']")).sendKeys(topic);
         driver.findElement(By.xpath("//div[@role='textbox']")).clear();
         driver.findElement(By.xpath("//div[@role='textbox']")).sendKeys(text);
         driver.findElement(By.xpath("//span[@title='Сохранить']")).click();
         driver.findElement(By.xpath("//button[@title='Закрыть']")).click();
+        driver.findElement(By.xpath("//a[@title='Черновики']")).click();
+
     }
 
-    @Test(description = "check Topic and Text", dataProvider = "createData", dataProviderClass = DataProviders.class, dependsOnMethods = {"createLetter"})
-    public void checkInfo(String addresser, String topic, String text){
-        WebDriver driver = ConnectionDriver.openConnection(WebBrowsers.CHROME);
+    @Test(description = "check Topic and Text", dependsOnMethods = {"createLetter"})
+    public void checkInfo() {
 
-        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
-        driver.findElement(By.xpath("//a[@title='Черновики']")).click();
-        String  topicLetter = driver.findElement(By.xpath("//span[@class='ll-sj__normal']")).getText();
-        String  textLetter = driver.findElement(By.xpath("//span[@class='ll-sp__normal']")).getText();
+        String topicLetter = driver.findElement(By.xpath("//span[@class='ll-sj__normal']")).getText();
+        String textLetter = driver.findElement(By.xpath("//span[@class='ll-sp__normal']")).getText();
         Assert.assertEquals(topic, topicLetter);
-        Assert.assertEquals(text,textLetter);
+        Assert.assertEquals(text, textLetter);
+
     }
 
     @Test(description = "logout", dependsOnMethods = {"checkInfo"})
-    public void logout(){
-        WebDriver driver = ConnectionDriver.openConnection(WebBrowsers.CHROME);
-
+    public void logout() {
         driver.findElement(By.id("PH_logoutLink")).click();
     }
 
-    @AfterClass ( alwaysRun = true, description = "close browser")
-    public void closeBrowser(){
+    @AfterClass(alwaysRun = true, description = "close browser")
+    public void closeBrowser() {
         ConnectionDriver.closeConnection();
     }
 }
